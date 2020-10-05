95fd99436ebf58a9907bfccc87c2f6c55e7492c4
Henrik <henrikdepre@gmail.com>
2020-10-05 22:26:32 +1100

    implemented Castle moves for black player

--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -3,9 +3,13 @@ package com.chess.engine.player;
 import com.chess.engine.Color;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Square;
 import com.chess.engine.pieces.Piece;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class BlackPlayer extends Player{
     public BlackPlayer(final Board board,
@@ -27,5 +31,37 @@ public class BlackPlayer extends Player{
     @Override
     public Player getOpponent(){
         return this.board.whitePlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(Collection<Move> playerLegals, Collection<Move> opponentLegals) {
+        final List<Move> kingCastles = new ArrayList<>();
+        if(this.playerKing.isFirstMove() && !this.isInCheck()){
+            // Black king side castle
+            if(!this.board.getSquare(5).isSquareOccupied() &&
+               !this.board.getSquare(6).isSquareOccupied()){
+                final Square rookSquare = this.board.getSquare(7);
+                if(rookSquare.isSquareOccupied() && rookSquare.getPiece().isFirstMove()){
+                    if(Player.calculateAttacksOnTile(5, opponentLegals).isEmpty() &&
+                            Player.calculateAttacksOnTile(6, opponentLegals).isEmpty() &&
+                            rookSquare.getPiece().getPieceType().isRook()){
+                    }
+                    //TODO: add a castle move
+                    kingCastles.add(null);
+                }
+            }
+            // Black queen side castle
+            if(!this.board.getSquare(1).isSquareOccupied() &&
+               !this.board.getSquare(2).isSquareOccupied() &&
+               !this.board.getSquare(3).isSquareOccupied()){
+
+                final Square rookSquare = this.board.getSquare(0);
+                if(rookSquare.isSquareOccupied() && rookSquare.getPiece().isFirstMove()){
+                    //TODO: add castle move
+                    kingCastles.add(null);
+                }
+            }
+        }
+        return Collections.unmodifiableList(kingCastles);
     }
 }
